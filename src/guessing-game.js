e608da0cf7c3e6e120e8e45d4e8834a3be165a10
Some Nick <somenick@example.com>
2023-02-20 20:11:16 +0300

    feat: implement task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 100;
+    this.current = 50;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.current = Math.round((this.max - this.min) / 2) + this.min;
+  }
 
-    }
+  guess() {
+    return this.current;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.current;
+    this.current = Math.round((this.max - this.min) / 2) + this.min;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.current;
+    this.current = Math.round((this.max - this.min) / 2) + this.min;
+  }
 }
 
 module.exports = GuessingGame;
